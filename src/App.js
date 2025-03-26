0af1e8cb24c954917b77302ce344f24ab393f759
Abin Johnson <dynamo.exe@proton.me>
2025-03-26 12:40:39 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
+function Post({ post, onDelete }) {
+  return(
+  <div className='post'>
+    <h2>{post.title}</h2>
+    <p>{post.content}</p>
+    <button onClick ={ () => {onDelete(post.id)}}>Delete</button>
+  </div>
+)}
+
 function App() {
+  const [posts, setPosts] = useState([]);
+  const [title, setTitle] = useState('');
+  const [content, setContent] = useState('');
+
+  useEffect (() => {
+    const savedPosts = JSON.parse(localStorage.getItem('posts')) || [];
+    setPosts(savedPosts);
+  }, [])
+  
+  useEffect(() => {
+    localStorage.setItem('posts', JSON.stringify(posts));
+  }, [posts])
+
+  const addPost = () => {
+    if (title.trim() && content.trim()) {
+      const newPost = { id: Date.now(), title, content};
+      setPosts([...posts, newPost]);
+      setTitle('');
+      setContent('');
+    }
+  }
+  const deletePost = (id) => {
+    setPosts(posts.filter((post) => post.id !== id))
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>Mini Blog</h1>
+      <input type='text' value={title} placeholder='Title' onChange={(e) => setTitle(e.target.value)} />
+      <textarea value={content} placeholder='Content of blog' onChange={(e) => setContent(e.target.value)} />
+      <button onClick={addPost}>Add Post</button>
+      <div className='posts'>
+        {posts.map((post) => (
+          <Post key={post.id} post={post} onDelete={deletePost}/>
+        ))}
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
